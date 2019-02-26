0a2fea5eb0240dd9539d691a583d8456bf35e1c8
Samuel Beca <samuel.beca@gmail.com>
2019-02-27 12:03:12 +1300

    added user and phrase CRUD routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  has_many :phrases
   validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
 
-  get 'phrases', to: 'phrases#index'
+  resources :phrases, only: [:create, :index, :show, :delete]
+  resources :users
 
-
-
+  # delete 'phrases/:id', to: 'phrases#destroy'
 end
