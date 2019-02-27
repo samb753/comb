77a8e4f9e791a46704ff5e6b79d88a08dfd6232e
Samuel Beca <samuel.beca@gmail.com>
2019-02-28 12:33:15 +1300

    added user model with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :phrases
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :password, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
