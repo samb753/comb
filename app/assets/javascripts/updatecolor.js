262574f3a3e3cd34d6b2f30596563aa4576eab20
Samuel Beca <samuel.beca@gmail.com>
2019-03-22 13:51:35 +0100

    moved form to bottom

--- app/assets/javascripts/updatecolor.js
+++ app/assets/javascripts/updatecolor.js
@@ -2,8 +2,6 @@ let colorInput = document.getElementById('phrase_color');
 
 colorInput.oninput = () => {
   console.log(colorInput.value);
-  document.getElementById('newphrasebox').style.backgroundColor = colorInput.value
+  document.getElementById('newPhraseBackground').style.backgroundColor = colorInput.value
 }
-
-
 
