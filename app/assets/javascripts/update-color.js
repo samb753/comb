d5e5efc0d6a04a61581edac530524a240bbfa90f
Samuel Beca <samuel.beca@gmail.com>
2019-04-03 13:51:16 +0200

    heroku test for mobile

--- app/assets/javascripts/update-color.js
+++ app/assets/javascripts/update-color.js
@@ -1,13 +1,13 @@
 document.addEventListener("turbolinks:load", function() {
   let colorInput = document.getElementById('phrase_color');
 
-  colorInput.onfocus = () => {
-    $('body').css('overflow', 'hidden');
-  }
+  // colorInput.onfocus = () => {
+  //   $('body').css('overflow', 'hidden');
+  // }
 
-   colorInput.onblur = () => {
-    $('body').css('overflow', 'auto');
-  }
+  //  colorInput.onblur = () => {
+  //   $('body').css('overflow', 'auto');
+  // }
 
   colorInput.oninput = () => {
     console.log(colorInput.value);
