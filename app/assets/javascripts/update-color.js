8dde103c7dede0aa1aebda2f3677a93ef6eaff65
Samuel Beca <samuel.beca@gmail.com>
2019-04-03 13:38:51 +0200

    updated js for minicolors so prevent body scrollig during input

--- app/assets/javascripts/update-color.js
+++ app/assets/javascripts/update-color.js
@@ -1,6 +1,14 @@
 document.addEventListener("turbolinks:load", function() {
   let colorInput = document.getElementById('phrase_color');
 
+  colorInput.onfocus = () => {
+    $('body').css('overflow', 'hidden');
+  }
+
+   colorInput.onblur = () => {
+    $('body').css('overflow', 'auto');
+  }
+
   colorInput.oninput = () => {
     console.log(colorInput.value);
     document.getElementById('newPhraseBackground').style.backgroundColor = colorInput.value
