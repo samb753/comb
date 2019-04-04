dbee7ed25113bc30d5a0a069d6079e96cd04fef7
Samuel Beca <samuel.beca@gmail.com>
2019-04-04 15:51:40 +0200

    changed alerts so navbar can still be clicked

--- db/migrate/20190226222814_create_phrases.rb
+++ db/migrate/20190226222814_create_phrases.rb
@@ -1,7 +1,6 @@
 class CreatePhrases < ActiveRecord::Migration[5.2]
   def change
     create_table :phrases do |t|
-      t.references :user, index: true, foreign_key: true
       t.string :first_word
       t.string :second_word
 
