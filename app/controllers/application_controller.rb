e914b282a8fbe8e1c890c8797fe80ba29be72fd3
Samuel Beca <samuel.beca@gmail.com>
2019-04-04 13:49:44 +0200

    added redirect to current user page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, only: :create
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end
