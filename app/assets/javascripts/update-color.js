f061243fab87d4d3f7a21ebb9742174c8ba1fd61
Samuel Beca <samuel.beca@gmail.com>
2019-04-10 12:19:45 +0200

    remove scroll js for heroku test

--- app/assets/javascripts/update-color.js
+++ app/assets/javascripts/update-color.js
@@ -1,16 +1,10 @@
 document.addEventListener("turbolinks:load", function() {
   let colorInput = document.getElementById('phrase_color');
-
-  colorInput.onfocus = () => {
-    $('body').addClass('no-scroll');
-  }
-
-   colorInput.onblur = () => {
-    $('body').removeClass('no-scroll');
-  }
-
   colorInput.oninput = () => {
     console.log(colorInput.value);
     document.getElementById('newPhraseBackground').style.backgroundColor = colorInput.value
   }
 })
+
+
+
