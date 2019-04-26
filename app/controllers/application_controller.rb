d9668656d1d922985c450038755f2777f899c6bf
Samuel Beca <samuel.beca@gmail.com>
2019-04-26 10:40:28 +0200

    changed redirect after sign in to go to index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, only: :create
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    phrases_path
   end
 end
